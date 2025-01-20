c466f79b081a87c89958f18bb6b01fcc5f8dd291
hmtamim199 <hmtamim8906@gmail.com>
2025-01-20 23:20:25 +0600

    added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">Login</a>
   </div>
 </div>
       
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({subeading, heading}) => {
   return (
-    <div className='mx-auto my-16 border-y-4 text-center'>
+    <div className='mx-auto text-center'>
       <p className='text-yellow-600 '>---{subeading}---</p>
-      <h3 className='text-3xl uppercase '>{heading}</h3>
+      <h3 className='text-3xl uppercase border-y-4 py-16  '>{heading}</h3>
     </div>
   );
 };
