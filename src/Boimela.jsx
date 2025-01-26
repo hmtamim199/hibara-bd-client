5d69627c7da0631cc115914c69baaf01c6fadce7
hmtamim199 <hmtamim8906@gmail.com>
2025-01-26 21:54:34 +0600

    boimela added

---
+++ src/Boimela.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// const Boimela = () => {
+//   return (
+//     <div>
+      
+//     </div>
+//   );
+// };
+
+// export default Boimela;
+
+const Boimela = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Boimela;
