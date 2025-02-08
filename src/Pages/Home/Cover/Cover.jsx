92ad6a2dfe989db20dda80ac093e473d15a137ce
hmtamim199 <hmtamim8906@gmail.com>
2025-02-08 22:45:33 +0600

    added usehook

--- src/Pages/Home/Cover/Cover.jsx
+++ src/Pages/Home/Cover/Cover.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import coverImg from '../../../assets/home/SAVE_20241210_192207.jpg'
+import coverImg from '../../../assets/home/banner.jpg'
 
 const Cover = () => {
   return (
