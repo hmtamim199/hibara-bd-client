7fe024e0be580bb5b58d444331062ff2ca0209f1
hmtamim199 <hmtamim8906@gmail.com>
2025-02-09 22:30:02 +0600

    added offered items

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,25 @@
 import Cover from "../Cover/Cover";
 import coverImg from '../../../assets/menu/banner3.jpg'
+import useHook from "../../../hooks/useHook";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 
 const Menu = () => {
+
+  const [menu] = useHook([]);
+
+  const desert = menu.filter( item => item.category === "dessert");
+  const salad = menu.filter( item => item.category === "salad");
+  const soup = menu.filter( item => item.category === "soup");
+  const pizza = menu.filter( item => item.category === "pizza");
+  const offered = menu.filter( item => item.category === "offered");
   return (
     
     <div>
       <Cover img={coverImg} title="our menu"></Cover>
-      <h2>this is menu of our bistro boss </h2>
+    <SectionTitle subeading="don't miss" heading="TODAY'S OFFER"></SectionTitle>
+    <MenuCategory items={offered}></MenuCategory>
     </div>
   );
 };
