4dcc323da4d1b7a4f8749b38dd5be1d2f08dd5a8
hmtamim199 <hmtamim8906@gmail.com>
2025-02-12 13:12:49 +0600

    react tabs added

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,8 +1,30 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
+import { useState } from "react";
+import Cover from "../Menu/Cover/Cover";
 
 const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0)
     return (
         <div>
+            <Cover></Cover>
+
+    
+  <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+    <TabList>
+      <Tab>Salads</Tab>
+      <Tab>pizza</Tab>
+      <Tab>Soup</Tab>
+      <Tab>Desert</Tab>
+      <Tab>Drinks</Tab>
+    </TabList>
+    <TabPanel></TabPanel>
+    <TabPanel></TabPanel>
+    <TabPanel></TabPanel>
+    <TabPanel></TabPanel>
+    <TabPanel></TabPanel>
+  </Tabs>
 
         </div>
     );
