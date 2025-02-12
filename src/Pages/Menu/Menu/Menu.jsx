a5140ae17dbdcce2957c4f1828926ca65c1b3e3a
hmtamim199 <hmtamim8906@gmail.com>
2025-02-12 13:48:42 +0600

    foodcart added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,26 +1,25 @@
 import Cover from "../Cover/Cover";
-import coverImg from '../../../assets/menu/banner3.jpg'
+import coverImg from "../../../assets/menu/banner3.jpg";
 import useHook from "../../../hooks/useHook";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuCategory from "../MenuCategory/MenuCategory";
 
-
 const Menu = () => {
-
-
   const [menu] = useHook([]);
 
-  const desert = menu.filter( item => item.category === "dessert");
-  const salad = menu.filter( item => item.category === "salad");
-  const soup = menu.filter( item => item.category === "soup");
-  const pizza = menu.filter( item => item.category === "pizza");
-  const offered = menu.filter( item => item.category === "offered");
+  const desert = menu.filter((item) => item.category === "dessert");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
-    
     <div>
       <Cover img={coverImg} title="our menu"></Cover>
-    <SectionTitle subeading="don't miss" heading="TODAY'S OFFER"></SectionTitle>
-    <MenuCategory items={offered}></MenuCategory>
+      <SectionTitle
+        subeading="don't miss"
+        heading="TODAY'S OFFER"
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
     </div>
   );
 };
--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,33 +1,38 @@
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useHook from "../../hooks/useHook";
 import { useState } from "react";
 import Cover from "../Menu/Cover/Cover";
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0)
-    return (
-        <div>
-            <Cover></Cover>
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useHook([]);
 
-    
-  <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-    <TabList>
-      <Tab>Salads</Tab>
-      <Tab>pizza</Tab>
-      <Tab>Soup</Tab>
-      <Tab>Desert</Tab>
-      <Tab>Drinks</Tab>
-    </TabList>
-    <TabPanel></TabPanel>
-    <TabPanel></TabPanel>
-    <TabPanel></TabPanel>
-    <TabPanel></TabPanel>
-    <TabPanel></TabPanel>
-  </Tabs>
+  const desert = menu.filter((item) => item.category === "dessert");
+  const salad = menu.filter((item) => item.category === "salad");
+  const soup = menu.filter((item) => item.category === "soup");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Cover></Cover>
 
-        </div>
-    );
+      <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salads</Tab>
+          <Tab>pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Desert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Order;
