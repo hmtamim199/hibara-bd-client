25086abaf5b6f1340a5bfcf2733f5a5e719cd5ae
hmtamim199 <hmtamim8906@gmail.com>
2025-02-17 19:09:54 +0600

    new photo added

--- src/hooks/useHook.jsx
+++ src/hooks/useHook.jsx
@@ -5,7 +5,7 @@ const useHook = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/menu")
+    fetch("menu.json")
       .then((res) => res.json())
       .then((data) => {
         setMenu(data);
